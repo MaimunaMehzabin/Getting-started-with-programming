41b17a16ac50284f54751504d886e2d515c9ee94
Maimuna <knbari88@gmail.com>
2025-01-09 22:33:24 +0600

    heh...

--- birthday.py
+++ birthday.py
@@ -1,3 +1,20 @@
+print("\033c")
 name1 = input("Enter your first friends Name : ")
 bday1 = input("Enter your first friends Birthday : ")
 print(f"My first friends name is {name1} \nAnd her birthday is on {bday1} \n\n")
+
+name2 = input("Enter your second friends Name : ")
+bday2 = input("Enter your second friends Birthday : ")
+print(f"My second friends name is {name2} \nAnd her birthday is on {bday2} \n\n")
+
+name3 = input("Enter your third friends Name : ")
+bday3 = input("Enter your friends Birthday : ")
+print(f"My third friends name is {name3} \nAnd her birthday is on {bday3} \n\n")
+
+name4 = input("Enter your fourth friends Name : ")
+bday4 = input("Enter your friends Birthday : ")
+print(f"My fourth friends name is {name4} \nAnd her birthday is on {bday4} \n\n")
+
+name5 = input("Enter your fifth friends Name : ")
+bday5 = input("Enter your friends Birthday : ")
+print(f"My fifth friends name is {name5} \nAnd her birthday is on {bday5} \n\n")
